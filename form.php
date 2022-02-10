1cdd4d5d72d8c7cf44599db232f5449bc635568f
andrey1303110 <andrey1303110@gmail.com>
2022-02-10 17:27:35 +0200

    fix form php

--- form.php
+++ form.php
@@ -1,14 +1,14 @@
 <?php
-isset($_POST['email']) ? $email = $_POST['email'] : exit();
-isset($_POST['subject']) ? $subject = $_POST['subject'] : exit();
-isset($_POST['text']) ? $text = $_POST['text'] : exit();
+isset($_POST['email']) ? $email = htmlspecialchars($_POST['email']) : exit();
+isset($_POST['subject']) ? $subject = htmlspecialchars($_POST['subject']) : exit();
+isset($_POST['text']) ? $text = htmlspecialchars($_POST['text']) : exit();
 
 $addres  = "info@amma.vc";
  
 $mes = "Message from site AMMA\nEmail: $email\nSubject: $subject\nText: $text";
  
 $send = mail ($addres,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
+
 ini_set('short_open_tag', 'On');
 header('Refresh: 3; URL=index.html');
 ?>

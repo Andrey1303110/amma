0e450942aac0f77b3f01d5928e503d9e5f0e6852
andrey1303110 <andrey1303110@gmail.com>
2022-02-14 13:30:56 +0200

    fix in form php

--- form.php
+++ form.php
@@ -5,9 +5,9 @@ isset($_POST['text']) ? $text = htmlspecialchars($_POST['text']) : exit();
 
 $addres = "info@amma.vc";
  
-$mes = "Message from site AMMA\nEmail: $email\nSubject: $subject\nText: $text";
+$mes = "Message from site AMMA\nFrom: $email\nSubject: $subject\nText: $text";
  
-$send = mail ($addres,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+$send = mail ($addres,$mes,"Content-type:text/plain; charset = utf-8");
 
 ini_set('short_open_tag', 'On');
 header('Refresh: 3; URL=index.html');
